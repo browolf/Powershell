1e1607ad0c1e3ff82d2baf213de36bdde906ce07
browolf <brother.wolf@gmail.com>
2024-06-26 15:51:28 +0100

    Create find_wrong_smtp.ps1

---
+++ find_wrong_smtp.ps1
@@ -0,0 +1,32 @@
+#This script finds users that have a proxy adress with SMTP (capitals) instead of lowercase. 
+#A mistake now corrected in the user creation script caused this. 
+#SMTP causes the proxy address to become their primary email address in Office365
+#the list wasn't that long so I manually fixed the users. 
+
+# Import the Active Directory module
+Import-Module ActiveDirectory
+
+# Define the OU and the domain
+$OU = "OU=azure,OU=staff,dc=yourdomain"
+
+# Get users in the specified OU
+$users = Get-ADUser -Filter * -SearchBase $OU -Properties proxyAddresses
+
+# Initialize an array to store users with SMTP in proxyAddresses
+$usersWithSMTP = @()
+
+# Loop through each user and check the proxyAddresses attribute
+foreach ($user in $users) {
+    if ($user.proxyAddresses) {
+        foreach ($address in $user.proxyAddresses) {
+            #Write-Output ($address)
+            if ($address -cmatch "^SMTP:") {
+                $usersWithSMTP += $user
+                break
+            }
+        }
+    }
+}
+
+# Output the list of users with SMTP in proxyAddresses
+$usersWithSMTP | Sort-Object Name | Select-Object Name, SamAccountName, proxyAddresses | Format-Table -AutoSize
